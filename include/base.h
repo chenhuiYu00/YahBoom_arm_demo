a77a38b5a35a6385365d3d2ee9e06a96ff132e32
yuchen <116358@qq>
2024-12-31 11:24:38 +0800

    First commit.

---
+++ include/base.h
@@ -0,0 +1,21 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class base; }
+QT_END_NAMESPACE
+
+class base : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    base(QWidget *parent = nullptr);
+    ~base();
+
+private:
+    Ui::base *ui;
+};
+#endif // BASE_H
---
+++ src/base.cpp
@@ -0,0 +1,15 @@
+#include "include/base.h"
+#include "./ui_base.h"
+
+base::base(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::base)
+{
+    ui->setupUi(this);
+}
+
+base::~base()
+{
+    delete ui;
+}
+
